29e18e9e429781f3f00b6378427b589325ae6ec6
Ankit Thakur <ankit.thakur@ditstek.com>
2023-07-31 01:01:37 +0530

    jest test cases

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const v1Routes = require('./Routes/index')
+const v1Routes = require('./Routes/export');
 let app = express();
 
 
 app.use(express.json());
 app.use('/api/v1', v1Routes);
 
-mongoose.connect('mongodb://localhost/usersDB');
-module.exports = app;
+const server = app.listen(3000, () => {
+  // console.log("listening on port: 3000");
+});
+
+// mongoose.connect('mongodb://localhost/usersDB').then(() => {
+//   //  console.log("connected to mongodb")
+// });
+module.exports = { app, server };
